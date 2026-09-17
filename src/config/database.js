1fc67101dc1d8f62c7da33eba7d514c530555e60
ManishKumarLal1 <106573768+ManishKumarLal1@users.noreply.github.com>
2026-09-17 15:54:26 +0530

    Debug Aiven PostgreSQL certificate

--- src/config/database.js
+++ src/config/database.js
@@ -4,11 +4,20 @@ const path = require("path");
 
 require("dotenv").config();
 
+const caPath = path.join(__dirname, "../../ca.pem");
+
+console.log("CA path:", caPath);
+console.log("CA exists:", fs.existsSync(caPath));
+
+if (fs.existsSync(caPath)) {
+  console.log("CA size:", fs.statSync(caPath).size);
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: true,
-    ca: fs.readFileSync(path.join(__dirname, "../../ca.pem")).toString(),
+    ca: fs.readFileSync(caPath, "utf8"),
   },
 });
 
