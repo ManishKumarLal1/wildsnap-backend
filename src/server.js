0e0185b47b7ad91b58b3fd695d93a3ef1e34c9ba
ManishKumarLal1 <106573768+ManishKumarLal1@users.noreply.github.com>
2026-09-17 14:43:00 +0530

    Prepare backend for deployment

--- src/server.js
+++ src/server.js
@@ -11,9 +11,9 @@ async function startServer() {
 
     console.log("Database connection successful");
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
+    app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
   } catch (error) {
     console.error("Failed to connect to database:", error);
     process.exit(1);
